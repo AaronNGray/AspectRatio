b3151d949ef4d5e062945b0a29c16e6c585d0ef6
Aaron Gray <aaronngray@gmail.com>
2024-05-29 01:54:35 +0100

    nearly there inversion logic wise.

--- src/AspectRatioSelector.tsx
+++ src/AspectRatioSelector.tsx
@@ -1,13 +1,18 @@
 import AspectRatio, {calculateAspectRatio} from './AspectRatio';
 import "./AspectRatioSelector.css";
 
-const Button = ({ selected, aspectRatio, onClick }: {selected:boolean, aspectRatio:AspectRatio, onClick: (ratio:AspectRatio) => void}) => {
+const Button = ({ selected, aspectRatio, onClick, colour = 'black' }: {selected:boolean, aspectRatio:AspectRatio, onClick: (ratio:AspectRatio) => void, colour:string}) => {
     return (
         <div
             className={`button`}
             onClick={() => onClick(aspectRatio)}
         >
-            <svg width="60px" height="40px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000" className="bi bi-aspect-ratio">
+            <svg width="70px" height="45px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000" className="bi bi-aspect-ratio">
+                <path
+                    d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5v-9zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"
+                    stroke={colour}
+                    fillRule="nonzero"
+                />
                 <defs>
                     <filter id="invert-alpha">
                         <feColorMatrix type="matrix" values="1 0 0 0 0 
@@ -17,27 +22,23 @@ const Button = ({ selected, aspectRatio, onClick }: {selected:boolean, aspectRat
                     </filter>
                 </defs>
                 <g
-                    transform="translate(0,-952.36223)" filter="url(#invert-alpha)"
-                ></g>
-                <path
-                    d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5v-9zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"
-                    stroke={selected ? 'black' : 'transparent'}
-                    fillRule="nonzero"
-                />
-                <path
-                    d="M2 4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H3v2.5a.5.5 0 0 1-1 0v-3zm12 7a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H13V8.5a.5.5 0 0 1 1 0v3z"
-                    fill={selected ? 'transparent' : 'black'}
-                />
-                <text
-                    x="50%"
-                    y="50%"
-                    dominantBaseline="middle"
-                    textAnchor="middle"
-                    fill={'#0'}
-                    fontSize="3pt"
+                    filter={ selected ? 'url(#invert-alpha)' : '' }
                 >
-                    {aspectRatio.toString()}
-                </text>
+                    <path
+                        d="M2 4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H3v2.5a.5.5 0 0 1-1 0v-3zm12 7a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H13V8.5a.5.5 0 0 1 1 0v3z"
+                        fill={colour}
+                    />
+                    <text
+                        x="50%"
+                        y="52%"
+                        dominantBaseline="middle"
+                        textAnchor="middle"
+                        fill={colour}
+                        fontSize="4pt"
+                    >
+                        {aspectRatio.toString()}
+                    </text>
+                </g>
             </svg>      
         </div>
     );
